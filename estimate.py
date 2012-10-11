26079541b3f58fd1a04c9f4ac84dfddf68808ea4
Andriy Khavryuchenko <akhavr@gmail.com>
2012-10-11 23:30:00 +0400

    Best parameter fit

--- estimate.py
+++ estimate.py
@@ -1,23 +1,31 @@
 import math
+import numpy as np
 from sklearn import cross_validation
+from sklearn.grid_search import GridSearchCV
 from sklearn import metrics
 from sklearn.svm import SVR
 
 from fun1 import get_data
 
-tickets, times = get_data()
+def learn():
+    tickets, times = get_data()
 
-tickets_train, tickets_test, times_train, times_test = \
-    cross_validation.train_test_split(tickets, times, 
-                                      test_size=0.2, random_state=0)
-clf = SVR()
-clf.fit(tickets_train, times_train)
+    tickets_train, tickets_test, times_train, times_test = \
+        cross_validation.train_test_split(tickets, times, 
+                                          test_size=0.2, random_state=0)
+    clf = GridSearchCV(estimator=SVR(), 
+                       param_grid=dict(C=np.logspace(-1,10,5)),
+                       n_jobs=-1)
+    clf.fit(tickets_train, times_train)
 
-times_train_predict = clf.predict(tickets_train)
-times_test_predict = clf.predict(tickets_test)
+    times_train_predict = clf.predict(tickets_train)
+    times_test_predict = clf.predict(tickets_test)
 
-train_error = metrics.mean_squared_error(times_train, times_train_predict)
-test_error = metrics.mean_squared_error(times_test, times_test_predict)
+    train_error = metrics.mean_squared_error(times_train, times_train_predict)
+    test_error = metrics.mean_squared_error(times_test, times_test_predict)
 
-print 'Train error: %.1f Test error: %.2f' % (
-    math.sqrt(train_error)/(24*3600), math.sqrt(test_error)/(24*3600))
+    print 'Train error: %.1f Test error: %.2f' % (
+        math.sqrt(train_error)/(24*3600), math.sqrt(test_error)/(24*3600))
+    print 'Best C: %.1e' % clf.best_estimator_.C
+
+learn()
