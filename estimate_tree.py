4fec1fe0b5769b0219e51cffc704b15f30a333ea
Andriy Khavryuchenko <akhavr@gmail.com>
2012-10-15 10:40:29 +0400

    Added kNN and Tree estimators

---
+++ estimate_tree.py
@@ -0,0 +1,31 @@
+import math
+import numpy as np
+from sklearn import cross_validation
+from sklearn.grid_search import GridSearchCV
+from sklearn import metrics
+from sklearn.tree import DecisionTreeRegressor
+
+from fun6 import get_data
+
+def learn(param_grid=None,):
+    tickets, times = get_data()
+
+    tickets_train, tickets_test, times_train, times_test = \
+        cross_validation.train_test_split(tickets, times, 
+                                          test_size=0.2, random_state=0)
+    clf = GridSearchCV(estimator=DecisionTreeRegressor(),
+                       param_grid=param_grid,
+                       n_jobs=-1)
+    clf.fit(tickets_train, times_train)
+
+    times_train_predict = clf.predict(tickets_train)
+    times_test_predict = clf.predict(tickets_test)
+
+    train_error = metrics.mean_squared_error(times_train, times_train_predict)
+    test_error = metrics.mean_squared_error(times_test, times_test_predict)
+
+    print 'Train error: %.1f Test error: %.2f' % (
+        math.sqrt(train_error)/(24*3600), math.sqrt(test_error)/(24*3600))
+    print 'Best depth: %.1e' % clf.best_estimator_.max_depth
+
+learn(param_grid=dict(max_depth=range(3,11)))
